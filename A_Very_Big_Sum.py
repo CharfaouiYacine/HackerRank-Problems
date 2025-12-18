43f29c93cb00e604193275a06d1170367d58ad6a
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2025-12-19 00:12:33 +0100

    a very big sum

---
+++ A_Very_Big_Sum.py
@@ -0,0 +1,16 @@
+def aVeryBigSum(ar):
+    sum = 0
+    for i in ar:
+        sum += i
+    return sum
+
+if __name__ == '__main__':
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    print(result)
+
