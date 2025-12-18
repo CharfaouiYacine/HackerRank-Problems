dcef143568816cd18f4935d51c7b3a31209b69db
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2025-12-18 21:28:31 +0100

    String split and join

---
+++ SplitAndJoin.py
@@ -0,0 +1,9 @@
+def split_and_join(line):
+    line1 = line.split(" ")
+    line1 = "-".join(line1)
+    return line1
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
