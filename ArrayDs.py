47ac4af10d9167647e1e8eaa287154bb3e6c567a
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-02-19 12:38:26 +0100

    reversing an array

---
+++ ArrayDs.py
@@ -0,0 +1,11 @@
+def reverseArray(a):
+    i = 0
+    j = len(a) -1
+    count = len(a) / 2
+    while i < count:
+        temp = a[i]
+        a[i] = a[j]
+        a[j] = temp
+        i += 1
+        j -= 1
+    return a
